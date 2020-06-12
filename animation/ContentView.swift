c7ac83e3696e78849810d08907a564c209ce12ab
ruchi <ruchi.m@eheuristic.com>
2020-06-12 09:50:00 +0530

    Initial Commit

---
+++ animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  animation
+//
+//  Created by eheuristic on 12/06/20.
+//  Copyright © 2020 ehs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
